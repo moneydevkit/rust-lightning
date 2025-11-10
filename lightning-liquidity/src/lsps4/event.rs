cf270e5f168b43388360d2f2229d11646664f607
Martin Saposnic <martinsaposnic@gmail.com>
2025-11-10 15:52:31 -0300

    pass channel count to openChannel on lsps4

--- lightning-liquidity/src/lsps4/event.rs
+++ lightning-liquidity/src/lsps4/event.rs
@@ -53,5 +53,7 @@ pub enum LSPS4ServiceEvent {
 		their_network_key: PublicKey,
 		/// The amount we need to forward to the client after opening.
 		amt_to_forward_msat: u64,
+		/// The number of channels currently open with the peer when the request is made.
+		channel_count: usize,
 	},
 }
